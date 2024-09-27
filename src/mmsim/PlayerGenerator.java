ad325129acbdb559a6e99de390c17d02c1c7bdcb
S1deprojects <182558216+S1deprojects@users.noreply.github.com>
2024-09-27 13:05:00 +0100

    removed redundant function

--- src/mmsim/PlayerGenerator.java
+++ src/mmsim/PlayerGenerator.java
@@ -1,19 +1,10 @@
 package mmsim;
 
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Set;
 import java.util.HashSet;
 
 public class PlayerGenerator {
     private final Set<Integer> names = new HashSet<>();
-
-    public void rngNameGen(int numNames) {
-        names.clear();
-        for (int i = 0; i < numNames; i++) {
-            names.add(i);
-        }
-    }
 
     // generates a base list of players up to a set amount, with 'skill' increasing by 0.01
     public Set<Player> GeneratePlayers(int count) {
@@ -25,10 +16,8 @@ public class PlayerGenerator {
         double defaultVolatility = 0.05;
         double defaultConfidence = 35.0;
 
-        rngNameGen(count);
-        for (Integer name : names) {
-
-            Player p = new Player(name, startingSkill, defaultELO, defaultVolatility, defaultConfidence);
+        for (int i = 1; i <= count; i++) {
+            Player p = new Player(i, startingSkill, defaultELO, defaultVolatility, defaultConfidence);
             playerList.add(p);
             startingSkill += 0.01;
         }
