674d5798f482c1548a5de49785b673678d3478ee
winston <hey@winston.sh>
2024-12-21 16:18:52 +0100

    build(nix): properly package for nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         "x86_64-linux"
       ];
       perSystem =
-        { pkgs, ... }:
+        { pkgs, self', ... }:
         {
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
@@ -26,6 +26,61 @@
               JAVA_HOME = "${pkgs.graalvm-ce}";
               GRAALVM_HOME = "${pkgs.graalvm-ce}";
             };
+          };
+          packages = {
+            default = self'.packages.mmsim-native;
+
+            mmsim = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+              pname = "mmsim";
+              version = "1.0-SNAPSHOT";
+
+              src = builtins.path {
+                path = ./.;
+                name = "mmsim";
+              };
+
+              nativeBuildInputs = [
+                pkgs.gradle
+                pkgs.makeWrapper
+              ];
+
+              mitmCache = pkgs.gradle.fetchDeps {
+                pkg = self'.packages.mmsim;
+                data = ./deps.json;
+              };
+
+              # this is required for using mitm-cache on Darwin
+              __darwinAllowLocalNetworking = true;
+
+              installPhase = ''
+                mkdir -p $out/{bin,share/mmsim}
+                cp build/libs/mmsim-${finalAttrs.version}.jar $out/share/mmsim
+
+                makeWrapper ${pkgs.jre}/bin/java $out/bin/mmsim \
+                  --add-flags "-jar $out/share/mmsim/mmsim-${finalAttrs.version}.jar"
+              '';
+
+              meta.sourceProvenance = with pkgs.lib.sourceTypes; [
+                fromSource
+                binaryBytecode # mitm cache
+              ];
+            });
+
+            mmsim-native =
+              let
+                jarPkg = self'.packages.mmsim;
+              in
+              pkgs.buildGraalvmNativeImage {
+                pname = "mmsim";
+                version = jarPkg.version;
+
+                src = "${jarPkg}/share/mmsim/mmsim-${jarPkg.version}.jar";
+
+                build-src = builtins.path {
+                  path = ./.;
+                  name = "mmsim";
+                };
+              };
           };
         };
     };
